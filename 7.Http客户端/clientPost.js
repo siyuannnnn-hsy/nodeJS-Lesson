4602031d122f9d52bd03718239c67a038e390c0d
siyuannnnn-hsy <1587935941@qq.com>
2019-10-09 11:15:57 +0800

    a

---
+++ 7.Http客户端/clientPost.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const querystring = require("querystring");
+
+var options = {
+    host : "localhost",
+    port : 8081,
+    method : "post"
+};
+var usename = process.argv[2];
+var pwd = process.argv[3];
+var postData = {useName : usename,pwd :pwd};
+postData = querystring.stringify(postData);
+var req = http.request(options,function(res){
+
+});
+
+req.write(postData);
+req.end();
